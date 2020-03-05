81f3b781a036896f99efe42764785cd6561387be
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-03-05 14:55:57 -0300

    added header and sign in components. And Sign in/up page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInPage from './pages/sign-in/sign-in.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -15,9 +17,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInPage} />
         <Route exact path='/hats' component={HatsPage} />
       </Switch>
     </div>
