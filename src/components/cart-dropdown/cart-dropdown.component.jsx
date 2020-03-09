32e0fc323b537afa0a67fd5afbd43f76915f194a
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-03-09 08:44:04 -0300

    added checkout page, checkout item component and updated cart to use selectors from reselect.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,11 +5,12 @@ import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.components';
 import CartItem from '../cart-item/cart-item.component';
+import { toogleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 import { createStructuredSelector } from 'reselect';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {
@@ -19,7 +20,14 @@ const CartDropdown = ({ cartItems, history }) => (
           (<span className='empty-message'>Your cart is empty</span>)
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>Go to checkout</CustomButton>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout')
+        dispatch(toogleCartHidden());
+      }}
+    >
+      Go to checkout
+    </CustomButton>
   </div>
 );
 
